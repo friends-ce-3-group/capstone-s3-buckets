61623d817528864b463b782b087d65468fd190dd
Justin Lim <goodaccoustics@hotmail.com>
2023-11-07 00:39:04 +0800

    remove acl settings

--- website/s3.tf
+++ website/s3.tf
@@ -5,18 +5,18 @@ resource "aws_s3_bucket" "s3_website" {
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "s3_bucket_ownership_controls" {
-  bucket = aws_s3_bucket.s3_website.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
+#resource "aws_s3_bucket_ownership_controls" "s3_bucket_ownership_controls" {
+#  bucket = aws_s3_bucket.s3_website.id
+#  rule {
+#    object_ownership = "BucketOwnerPreferred"
+#  }
+#}
 
-resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-  depends_on = [ aws_s3_bucket_ownership_controls.s3_bucket_ownership_controls]
-  bucket = aws_s3_bucket.s3_website.id
-  acl = "private"
-}
+#resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+#  depends_on = [ aws_s3_bucket_ownership_controls.s3_bucket_ownership_controls]
+#  bucket = aws_s3_bucket.s3_website.id
+#  acl = "private"
+#}
 
 
 resource "aws_s3_bucket_website_configuration" "website" {
