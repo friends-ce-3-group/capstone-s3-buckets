d905c6d4bc2b3eae50a81f7fc77d5134ce3a79a0
Justin Lim <goodaccoustics@hotmail.com>
2023-11-08 00:02:32 +0800

    setup apigateway to s3 integration with terraform

--- images/apigateway.tf
+++ images/apigateway.tf
@@ -42,3 +42,72 @@ resource "aws_iam_role_policy_attachment" "api_gateway_role_s3_policy_attachment
   policy_arn = aws_iam_policy.s3_images_policy.arn
   role       = aws_iam_role.api_gateway_iam_role.name
 }
+
+# create a api gateway with the type of REST API
+resource "aws_api_gateway_rest_api" "image_upload_api" {
+  name = "${var.proj_name}-upload-api"
+  binary_media_types = [
+    "image/jpeg",
+    "image/jpg",
+    "image/png",
+    "image/gif"
+  ]
+}
+
+# create a resource path for the REST API /{bucket}/
+resource "aws_api_gateway_resource" "bucket" {
+  rest_api_id = aws_api_gateway_rest_api.image_upload_api.id
+  parent_id   = aws_api_gateway_rest_api.image_upload_api.root_resource_id
+  path_part   = "{bucket}"
+}
+
+# create a resource path for the REST API /{bucket}/{folder}
+resource "aws_api_gateway_resource" "folder" {
+  rest_api_id = aws_api_gateway_rest_api.image_upload_api.id
+  parent_id   = aws_api_gateway_resource.bucket.id
+  path_part   = "{folder}"
+}
+
+# create a resource path for the REST API /{bucket}/{folder}/{filename}
+resource "aws_api_gateway_resource" "filename" {
+  rest_api_id = aws_api_gateway_rest_api.image_upload_api.id
+  parent_id   = aws_api_gateway_resource.folder.id
+  path_part   = "{filename}"
+}
+
+# create a PUT method for the REST API
+resource "aws_api_gateway_method" "put_method" {
+  rest_api_id   = aws_api_gateway_rest_api.image_upload_api.id
+  resource_id   = aws_api_gateway_resource.filename.id
+  http_method   = "PUT"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.path.bucket" = true,
+    "method.request.path.folder" = true,
+    "method.request.path.filename" = true,
+  }
+}
+
+# configure integration between api gateway and s3
+resource "aws_api_gateway_integration" "s3_integration" {
+  type                    = "AWS"
+  rest_api_id             = aws_api_gateway_rest_api.image_upload_api.id
+  resource_id             = aws_api_gateway_resource.filename.id
+  http_method             = aws_api_gateway_method.put_method.http_method
+  integration_http_method = "PUT"
+  credentials             = aws_iam_role.api_gateway_iam_role.arn
+  #uri                     = "arn:aws:apigateway:${var.region}:s3:path//"
+  uri                     = "arn:aws:apigateway:${var.region}:s3:path/{bucket}/{folder}/{filename}"
+  request_parameters = {
+    "integration.request.path.bucket" = "method.request.path.bucket",
+    "integration.request.path.folder" = "method.request.path.folder",
+    "integration.request.path.filename" = "method.request.path.filename",
+  }
+
+}
+
+resource "aws_api_gateway_deployment" "api-deployment" {
+  depends_on  = [aws_api_gateway_integration.s3_integration]
+  rest_api_id = aws_api_gateway_rest_api.image_upload_api.id
+  stage_name  = "dev"
+}
