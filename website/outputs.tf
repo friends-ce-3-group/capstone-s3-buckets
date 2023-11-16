2345539298bd42373300784829452b2d428986a8
Justin Lim <goodaccoustics@hotmail.com>
2023-11-17 03:43:00 +0800

    update s3 region to us-west-2

--- website/outputs.tf
+++ website/outputs.tf
@@ -1,3 +1,8 @@
 output "s3_bucket_website_domain_name" {
   value = aws_s3_bucket_website_configuration.website.website_endpoint
 }
+
+resource "local_file" "write_url" {
+  content  = "S3_BUCKET_WEBSITE_URL=${aws_s3_bucket_website_configuration.website.website_endpoint}"
+  filename = "${path.module}/s3_bucket_website_url.dat"
+}
