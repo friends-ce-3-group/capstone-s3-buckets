fd55e57078901347fb1f1b8c9dd328602c150df0
Justin Lim <goodaccoustics@hotmail.com>
2023-11-17 09:08:37 +0800

    output to organization variables

--- images/outputs.tf
+++ images/outputs.tf
@@ -2,7 +2,21 @@ output "s3_bucket_images_domain_name" {
   value = aws_s3_bucket.s3_images.bucket_domain_name
 }
 
+output "s3_bucket_images_regional_domain_name" {
+  value = aws_s3_bucket.s3_images.bucket_regional_domain_name
+}
+
 output "api_gateway_invoke_url" {
   value = aws_api_gateway_deployment.api-deployment.invoke_url
+}
+
+resource "local_file" "write_images_url" {
+  content  = "S3_BUCKET_IMAGES_URL=${aws_s3_bucket.s3_images.bucket_regional_domain_name}"
+  filename = "${path.module}/s3_bucket_images_url.dat"
+}
+
+resource "local_file" "write_upload_images_url" {
+  content  = "API_GATEWAY_UPLOAD_IMAGES_URL=${aws_api_gateway_deployment.api-deployment.invoke_url}"
+  filename = "${path.module}/api_gateway_upload_images_url.dat"
 }
 
