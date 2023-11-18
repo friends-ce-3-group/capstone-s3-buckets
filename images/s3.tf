8424e4167c0fa638d8c75d5c6a4e754a82522885
Justin Lim <goodaccoustics@hotmail.com>
2023-11-18 18:04:47 +0800

    add cloudfront bucket policy to images bucket

--- images/s3.tf
+++ images/s3.tf
@@ -20,6 +20,27 @@ resource "aws_s3_object" "resized" {
   key    = "${var.resized_folder}/"
 }
 
+data "aws_iam_policy_document" "cloudfront" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${var.proj_name}/*"]
+    condition {
+      test     = "StringLike"
+      variable = "AWS:SourceArn"
+      values   = ["arn:aws:cloudfront::255945442255:distribution"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "images_bucket_policy" {
+  bucket = aws_s3_bucket.s3_images.id
+  policy = data.aws_iam_policy_document.s3_images_policy_document.json
+}
+
 resource "aws_s3_bucket_notification" "eventbridge_notification" {
   bucket      = aws_s3_bucket.s3_images.id
   eventbridge = true
